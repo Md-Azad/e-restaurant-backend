447723158121b94bb7d3503dea98dc4c2c71bfbf
Md-Azad <sokalazad@gmail.com>
2023-11-05 21:01:05 +0100

    besic backend setup added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express());
+
+app.get('/',(req,res)=>{
+    res.send("boss is running");
+})
+
+app.listen(port,()=>{
+    console.log(`the app is running of port: ${port}`);
+})
