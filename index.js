642f81732fdf72409fc04ac75dd2d645a7a9038a
Md-Azad <sokalazad@gmail.com>
2023-11-13 01:10:26 +0100

    created delete api to delete data from carts collection

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const user = [
 
 
 // db connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cn37c5v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -63,6 +63,15 @@ async function run() {
       const item = req.body;
       console.log("geting ing",item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    // delete data from api
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
     
