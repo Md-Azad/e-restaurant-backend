c86b3903631f4fb9d716557841f327305e4df962
Md-Azad <sokalazad@gmail.com>
2023-11-18 18:05:49 +0100

    created addItem api

--- index.js
+++ index.js
@@ -126,6 +126,12 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/menu',async(req,res)=>{
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    })
+
     // Review related apis
     app.get('/review',async(req,res)=>{
       const result = await reviewCollection.find().toArray();
