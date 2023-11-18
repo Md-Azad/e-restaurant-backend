b070cdfab612375ac20ae1b569c0c71567e8c307
Md-Azad <sokalazad@gmail.com>
2023-11-18 21:23:44 +0100

    secured the post menu api with verify JWT and verify Admin middleware

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/menu',async(req,res)=>{
+    app.post('/menu', verifyJWT,veryfyAdmin, async(req,res)=>{
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
       res.send(result);
